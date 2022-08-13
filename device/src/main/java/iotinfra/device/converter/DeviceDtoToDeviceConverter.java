89e9a83d16de2e9c164d130efcba9c9cf93a5c5b
Jacky Liu <Jacky35liu@gmail.com>
2022-08-13 11:43:18 -0500

    add device dto converter

--- device/src/main/java/iotinfra/device/converter/DeviceDtoToDeviceConverter.java
+++ device/src/main/java/iotinfra/device/converter/DeviceDtoToDeviceConverter.java
@@ -1,4 +1,19 @@
 package iotinfra.device.converter;
 
-public class DeviceDtoToDeviceConverter {
+import iotinfra.device.dto.DeviceDto;
+import iotinfra.device.model.Device;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeviceDtoToDeviceConverter implements Converter<DeviceDto, Device> {
+
+	@Override
+	public Device convert(DeviceDto deviceDto) {
+		Device device = new Device();
+		device.setDescription(deviceDto.getDescription());
+		device.setPosition(deviceDto.getPosition());
+		device.setName(deviceDto.getName());
+		return device;
+	}
 }
